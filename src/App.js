311c8895b0f63ea6b1ea291274a9988214c40a6e
Mikko Karvonen <mkarvonen34@gmail.com>
2019-10-25 19:34:05 +0300

    Add ability to start daily
    Time is still only a placeholder.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './App.css';
 
 function App() {
   const [people, setPeople] = useState(5);
+  const [daily, setDaily] = useState(false);
 
   return (
     <div className="App">
-      <h1>Daily Timer</h1>
-      <div class="controls">
-        <button onClick={() => {
-          if (people > 3) setPeople(people - 1)
-        }}>-</button>
-        {people}
-        <button onClick={() => {
-          if (people < 10) setPeople(people + 1)
-        }}>-</button>
-      </div>
-      <button>Start</button>
+      {daily ?
+        <div>
+          <h1>Daily is running</h1>
+          <p>10:00</p>
+          <button onClick={() => setDaily(!daily)}>Stop</button>
+        </div>
+      :
+        <div>
+          <h1>Daily Timer</h1>
+          <div class="controls">
+            <button onClick={() => {
+              if (people > 3) setPeople(people - 1)
+            }}>-</button>
+            {people}
+            <button onClick={() => {
+              if (people < 10) setPeople(people + 1)
+            }}>-</button>
+          </div>
+          <button onClick={() => setDaily(!daily)}>Start</button>
+        </div>
+      }
     </div>
   );
 }
