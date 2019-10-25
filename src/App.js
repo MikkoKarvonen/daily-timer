d014268655cdcb17148bf3fe1cb999c19340d855
Mikko Karvonen <mkarvonen34@gmail.com>
2019-10-25 19:28:15 +0300

    Add ability to add or remove team members
    At least for now the range is 3-10.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(5);
+
   return (
     <div className="App">
       <h1>Daily Timer</h1>
       <div class="controls">
-        <button>-</button>
-        5
-        <button>+</button>
+        <button onClick={() => {
+          if (people > 3) setPeople(people - 1)
+        }}>-</button>
+        {people}
+        <button onClick={() => {
+          if (people < 10) setPeople(people + 1)
+        }}>-</button>
       </div>
       <button>Start</button>
     </div>
