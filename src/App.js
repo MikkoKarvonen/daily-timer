4e29cd82a0701dfdbb01d23e8559cbc1cea4bf69
Mikko Karvonen <mkarvonen34@gmail.com>
2019-11-12 07:43:22 +0200

    Start timer when start is pressed
    The time is currently on three seconds for testing.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, {useState} from 'react';
 import './App.css';
+import Countdown, {zeroPad} from 'react-countdown-now';
 
 function App() {
   const [people, setPeople] = useState(5);
   const [daily, setDaily] = useState(false);
 
+  const Completionist = () => <h1>Daily is over.</h1>;
+
+  const renderer = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      return <Completionist />;
+    } else {
+      return (
+        <div>
+          <h1>Daily is running</h1>
+          <span>{zeroPad(minutes, 2)}:{zeroPad(seconds, 2)}</span>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="App">
       {daily ?
         <div>
-          <h1>Daily is running</h1>
-          <p>10:00</p>
+          <Countdown 
+            date={Date.now() + /*10 * 60 **/ 3 * 1000} 
+            renderer = {renderer}
+             >
+            <Completionist/>
+          </Countdown>
           <button onClick={() => setDaily(!daily)}>Stop</button>
         </div>
       :
